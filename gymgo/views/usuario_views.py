3a8a8fc43ac9ea013b1028fea5bac32b3be7ffd0
jonas <jonas.peixoto.087@gmail.com>
2024-01-09 11:11:25 -0300

    organizando views

--- gymgo/views/usuario_views.py
+++ gymgo/views/usuario_views.py
@@ -1,4 +1,3 @@
-
 from ..services import usuario_service
 from ..serializers import usuario_serializer
 from ..models import Usuario
@@ -10,40 +9,45 @@ from django.shortcuts import get_object_or_404
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-
-@api_view(['POST'])
-def login (request):
-    user = get_object_or_404(Usuario, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found"}, status=status.HTTP_400_BAD_REQUEST)
-    token, create = Token.objects.get_or_create(user=user)
-    serializer = usuario_serializer.UsuarioSerializer(instance=user)
-    return Response({"token": token.key, "user":serializer.data},status=status.HTTP_201_CREATED)
-            
-        
 @api_view(['POST'])
 def cadastro (request):
     serializer = usuario_serializer.UsuarioSerializer(data=request.data) 
+
     if serializer.is_valid():
         serializer.save()
         user = Usuario.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        print("teste")
         return Response({"token": token.key, "user":serializer.data},status=status.HTTP_201_CREATED)
+    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login (request):
+    username = request.data['username']
+    password = request.data['password']
+    user = get_object_or_404(Usuario, username=username)
+
+    if not user.check_password(password):
+        return Response({"detalhe": "Credenciais invalidas"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, create = Token.objects.get_or_create(user=user)
+    serializer = usuario_serializer.UsuarioSerializer(instance=user)
+    return Response({"token": token.key, "user":serializer.data},status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def perfil(request):
-    profile_data = {'usuario': request.user.id, **request.data}
-    serializer = usuario_serializer.PerfilUsuarioSerializer(data=profile_data)
+    dados_perfil = {'usuario': request.user.id, **request.data}
+    serializer = usuario_serializer.PerfilUsuarioSerializer(data=dados_perfil)
+
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"nome":request.user.username, "perfil_usuario":serializer.data, }, status=status.HTTP_201_CREATED)
     else:
         return Response({'detail': 'Usuário não autenticado'}, status=status.HTTP_401_UNAUTHORIZED)
 
