58bf4430ac62b00fecb4599159796dc253d7e74f
jonaspeixoto <jonas.peixoto.087@gmail.com>
2024-01-23 10:56:07 -0300

    Feat: Implementando documentação para funcionalidades dos usuarios

--- gymgo/views/usuario_views.py
+++ gymgo/views/usuario_views.py
@@ -11,6 +11,16 @@ from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 def cadastro (request):
+    """
+    Realiza o cadastro de um novo usuário.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição POST.
+
+    Returns:
+    - Response: Retorna um token de autenticação e os dados do usuário cadastrado.
+
+    """
     serializer = usuario_serializer.UsuarioSerializer(data=request.data) 
 
     if serializer.is_valid():
@@ -25,6 +35,16 @@ def cadastro (request):
 
 @api_view(['POST'])
 def login (request):
+    """
+    Realiza o login de um usuário existente.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição POST.
+
+    Returns:
+    - Response: Retorna um token de autenticação e os dados do usuário logado.
+    
+    """
     username = request.data['username']
     password = request.data['password']
     user = get_object_or_404(Usuario, username=username)
@@ -41,6 +61,16 @@ def login (request):
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def logout (request):
+    """
+    Realiza o logout do usuário autenticado.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição POST.
+
+    Returns:
+    - Response: Retorna uma mensagem indicando que o logout foi bem sucedido.
+    """
+
     request.auth.delete()
     return Response({"detalhes": "Logout bem sucedido"}, status=status.HTTP_200_OK )
 
@@ -48,6 +78,15 @@ def logout (request):
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def perfil_cadastro(request):
+    """
+    Realiza o cadastro do perfil de um usuário autenticado.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição POST.
+
+    Returns:
+    - Response: Retorna os dados do perfil cadastrado.
+    """
     dados_perfil = {'usuario': request.user.id, **request.data}
     serializer = usuario_serializer.PerfilUsuarioSerializer(data=dados_perfil)
 
@@ -62,6 +101,15 @@ def perfil_cadastro(request):
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def perfil_usuario(request):
+    """
+    Realiza o cadastro do perfil de um usuário autenticado.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição POST.
+
+    Returns:
+    - Response: Retorna os dados do perfil cadastrado.
+    """
     usuario = Usuario.objects.get(id=request.user.id)
     perfil_usuario = Perfil.objects.get(id=request.user.id)
     serializer_usuario = usuario_serializer.UsuarioSerializer(usuario)
@@ -74,6 +122,15 @@ def perfil_usuario(request):
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def fazer_checkin(request):
+    """
+    Realiza o check-in do aluno associado ao usuário autenticado.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição POST.
+
+    Returns:
+    - Response: Retorna os dados do check-in realizado.
+    """
     dados_checkin = {'aluno': request.user.id}
     serializer = usuario_serializer.CheckInUsuarioSerializer(data=dados_checkin)
     print(serializer)
@@ -84,12 +141,18 @@ def fazer_checkin(request):
         return Response({'detail': 'Usuário não autenticado'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-
-    
-
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
+    """
+    Testa a autenticação e retorna uma mensagem indicando que o usuário está autenticado.
+
+    Parameters:
+    - request: Objeto contendo os dados da requisição GET.
+
+    Returns:
+    - Response: Retorna uma mensagem indicando que o usuário está autenticado.
+    """
     return Response(f'usuario autenticado {request.user.email}')
 
